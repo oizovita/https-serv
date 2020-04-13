28ca95e7c1045fd47deba464256c31979620f588
Oles <oizovita@yahoo.com>
2020-04-13 22:59:09 +0300

    first commit'

---
+++ ubuntu-docker-install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+apt-cache policy docker-ce
+
+sudo apt-get install -y docker-ce mc htop 
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo usermod -aG docker $(whoami)

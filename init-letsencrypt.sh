61ae0e39f75d7a2069fc15c792d40889ad49aa36
Oles <oizovita@yahoo.com>
2020-04-15 21:58:56 +0300

    fix

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(example.org www.example.org)
+domains=(oles.ninja www.oles.ninja)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
